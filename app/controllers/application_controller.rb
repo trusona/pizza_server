327c96f4ed06f461ffa4a1b099c1bc52b4a81d64
Chris Young <chrisyoung@MacBook-Pro.local>
2016-03-20 21:20:59 -0700

    no need for this anymore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-
-  private
-
-  def commands
-    @commands ||= {
-      get_pizzas:     Commands::GetPizzas.new(repo: Repositories::Couch::Pizza),
-      create_pizza:   Commands::CreatePizza.new(repo: Repositories::Couch::Pizza),
-      get_toppings:   Commands::GetToppings.new(repo: Repositories::AR::Topping),
-      create_topping: Commands::CreateTopping.new(repo: Repositories::AR::Topping),
-    }
-  end
 end
