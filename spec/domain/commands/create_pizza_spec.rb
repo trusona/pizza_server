38f0dc1f8dfaa8445628920294682cc4a0df2e5e
Chris Young <chrisyoung@MacBook-Pro.local>
2016-05-06 13:56:32 -0700

    make create pizza spec more readable

--- spec/domain/commands/create_pizza_spec.rb
+++ spec/domain/commands/create_pizza_spec.rb
@@ -1,30 +1,33 @@
 require "rails_helper"
 
 RSpec.describe Commands::CreatePizza do
-  subject { described_class.new }
+  subject     { described_class.new }
+  let(:pizza) { subject.call(pizza: attributes) }
+  let(:attributes) do
+    { 
+      "name"        => "Sicilian",
+      "description" => "Thick Crust",
+      "toppings"    => ["mushrooms", "corn husks"]
+    }
+  end
+  let(:attributes_with_missing_name) do
+    { 
+      "description" => "Thick Crust",
+      "toppings"    => ["mushrooms", "corn husks"]
+    }
+  end
+
   describe '#call' do
-    let(:attributes) do
-      { "name"        => "Sicilian",
-        "description" => "Thick Crust",
-        "toppings"    => ["mushrooms", "corn husks"]}
-    end
-
-    let(:pizza) { subject.call(pizza: attributes) }
-
     it 'creates a pizza with an id' do
       expect(pizza).to eq Models::Pizza.new(attributes.merge("id" => 1))
     end
 
     describe 'Errors' do
-      let(:attributes_with_missing_name) do
-        { "description" => "Thick Crust",
-          "toppings"    => ["mushrooms", "corn husks"]}
-      end
-
       it 'blows up if a validation breaks' do
-        expect { subject.call(pizza: attributes_with_missing_name) }.to raise_error(Validators::ValidationError) { |error|
+        expect { subject.call(pizza: attributes_with_missing_name) }
+          .to raise_error(Validators::ValidationError) do |error|
           expect(error.errors).to eq :name => ["can't be blank"]
-        }
+        end
       end
     end
   end
