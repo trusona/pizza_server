155142a3cfc3f60e0c7c86382f15fdce4a8b5ef0
Chris Young <chrisyoung@MacBook-Pro.local>
2016-05-29 15:46:06 -0700

    dont inherit

--- app/domain/repositories/pizza.rb
+++ app/domain/repositories/pizza.rb
@@ -1,9 +1,30 @@
 module Repositories
-  class Pizza < Base
+  class Pizza
   	@collection = {}
-    
-    def self.model
-      Models::Pizza
+  
+    def self.create attributes
+      next_id = @collection.keys.length + 1
+      @collection[next_id] = Models::Pizza.new(attributes.merge("id" => next_id))
+    end
+
+    def self.read id
+      @collection[id]
+    end
+
+    def self.update id, attributes
+      # NOT IMPLEMENTED
+    end
+
+    def self.delete(id)
+      # NOT IMPLEMENTED
+    end
+
+    def self.list
+      @collection.values
+    end
+
+    def self.reset
+      @collection = {}
     end
   end
 end
--- app/domain/repositories/topping.rb
+++ app/domain/repositories/topping.rb
@@ -1,8 +1,30 @@
 module Repositories
-  class Topping < Base
+  class Topping
   	@collection = {}
-    def self.model
-      Models::Topping
+  
+    def self.create attributes
+      next_id = @collection.keys.length + 1
+      @collection[next_id] = Models::Topping.new(attributes.merge("id" => next_id))
+    end
+
+    def self.read id
+      @collection[id]
+    end
+
+    def self.update id, attributes
+      # NOT IMPLEMENTED
+    end
+
+    def self.delete(id)
+      # NOT IMPLEMENTED
+    end
+
+    def self.list
+      @collection.values
+    end
+
+    def self.reset
+      @collection = {}
     end
   end
 end
